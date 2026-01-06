a3f476f9fd54d8cd0d3bf8328343e253f0008c31
Rawan Bazzi <rawan.bazzi@net.usj.edu.lb>
2026-01-06 15:32:49 +0200

    End of Section 12

--- API/Program.cs
+++ API/Program.cs
@@ -5,10 +5,22 @@ using Application.Core;
 using FluentValidation;
 using Application.Activities.Validators;
 using API.Middleware;
-
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authorization;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+}
+
+
+);
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
@@ -21,24 +33,34 @@ builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
 builder.Services.AddTransient<ExceptionMiddleware>();
-
+builder.Services.AddIdentityApiEndpoints<User>(
+    opt=>
+    {
+        opt.User.RequireUniqueEmail = true;
+    }
+).AddRoles<IdentityRole>()
+ .AddEntityFrameworkStores<AppDbContext>();
 var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 //app.UseRouting();
 app.UseCors(x => 
     x.AllowAnyHeader()
      .AllowAnyMethod()
+     .AllowCredentials()
      .WithOrigins("http://localhost:3000"));
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
-
+app.MapGroup("api").MapIdentityApi<User>();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 try
 {
     var context= services.GetRequiredService<AppDbContext>();
+    var userManager = services.GetRequiredService<UserManager<User>>();
 
     await context.Database.MigrateAsync();
-    await DbInitializer.SeedData(context);
+    await DbInitializer.SeedData(context, userManager);
 }
 catch (Exception ex)
 {
